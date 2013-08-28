46368e502ec7c40834be023f9793100f251d5723
Vampire <zuncity@shisontekiMacBook-Air.local>
2013-08-28 20:29:22 +0800

    Initial Commit

---
+++ sdliser/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  sdliserAppController.h
+//  sdliser
+//
+//  Created by Vampire on 13-8-28.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ sdliser/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  sdliserAppController.h
+//  sdliser
+//
+//  Created by Vampire on 13-8-28.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
